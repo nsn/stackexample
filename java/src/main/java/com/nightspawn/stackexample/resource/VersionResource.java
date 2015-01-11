65293fdf27c66d623abc7f7c884bfdff87692c13
michael.bayer <michael.bayer@gameforge.com>
2015-01-11 19:59:31 +0100

    version resource now returns version from pom.xml

--- java/src/main/java/com/nightspawn/stackexample/resource/VersionResource.java
+++ java/src/main/java/com/nightspawn/stackexample/resource/VersionResource.java
@@ -1,18 +1,37 @@
 package com.nightspawn.stackexample.resource;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.ejb.Stateless;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.WebApplicationException;
 
 /**
- *
+ * retrieve api version information
  */
 @Stateless
 @Path("/version")
 public class VersionResource {
 
+    /**
+     * reads version information from config.properties
+     * 
+     * @return a String containing version information
+     */
     @GET
     public String getVersion() {
-        return "0.1";
+        try {
+            InputStream in = getClass().getResourceAsStream("/config.properties");
+            Properties prop = new Properties();
+            prop.load(in);
+            String versionStr = prop.getProperty("version");
+            return versionStr;
+        } catch (IOException e) {
+            // throw new ServiceException(MSG, e);
+            throw new WebApplicationException(500);
+        }
     }
 }
